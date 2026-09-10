b81cbc6751724a50356022ee12c66f209c76f636
tanmay-dagaonkar <56819787+tanmay-dagaonkar@users.noreply.github.com>
2026-09-11 01:14:59 +0530

    Fix Pages build configuration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,1 +1,7 @@
-const config = {\n  plugins: {\n    "@tailwindcss/postcss": {},\n  },\n};\n\nexport default config;\n
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+
+export default config;
